74c30f54fcc7520f975ce37bc276d9a2604e1bd5
Steve Agalloco <steve.agalloco@gmail.com>
2009-05-21 10:11:23 -0400

    modified install script to backup existing files to a directory when creating symlinks

--- install.rb
+++ install.rb
@@ -4,9 +4,22 @@
 
 home = File.expand_path('~')
 
+
 Dir['*'].each do |file|
+  # skip if install.rb or README
   next if file =~ /install|README/
-  target = File.join(home, ".#{file}")
+
+  symfile = ".#{file}"
+  target = File.join(home, symfile)
+
+  # setup backup directory
+  backup = File.join(home, ".olddotfiles")
+  `mkdir -p #{backup}`
+  
+  # backup the file if it exists and it's not a symlink
+  `mv #{target} #{File.join(backup, symfile)}` if File.exist?(target) and !File.symlink?(target)
+  
+  # create a symbolic link
   `ln -s #{File.expand_path file} #{target}`
 end
 
